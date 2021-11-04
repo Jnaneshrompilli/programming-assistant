65d2c1f642c555a92ce7f0a99d840432b939600a
Jnaneswar <Jnaneshvsjnani@gmail.com>
2021-11-04 11:15:34 +0530

    Resize cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,9 @@ import "./card.css"
 export default function Card() {
     return (
         <div className="card">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/1/16/Ubuntu_and_Ubuntu_Server_Icon.png" alt="Avatar" />
+            <div className="card-image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/1/16/Ubuntu_and_Ubuntu_Server_Icon.png" alt="Avatar" />
+            </div>
             <div className="card-container">
                 <h2>How to install WSL on windows 11</h2>
             </div>
